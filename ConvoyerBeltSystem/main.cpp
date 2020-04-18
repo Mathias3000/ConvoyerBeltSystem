f5dd668932be83dfa5e6d006f00c754b03973d74
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-04-18 10:17:50 +0200

    minor changes in main
    Just added some comments + TODOs for future

--- ConvoyerBeltSystem/main.cpp
+++ ConvoyerBeltSystem/main.cpp
@@ -3,5 +3,7 @@
 int main()
 {
     printf("hello from ConvoyerBeltSystem!\n");
+    // start state maschine 
+    // TODO: see how to implement in the best way. Don't use god-classes: refer to OO design patterns
     return 0;
 }
