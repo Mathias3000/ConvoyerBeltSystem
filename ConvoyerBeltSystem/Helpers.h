547bfcb9c944de265390babaf99db6142682055c
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-04-19 16:57:48 +0200

    Added Utils folder for helper and test functions
    Any other functions which might be needed during implementation can be added here. Test functions are also located in the Utils folder

---
+++ ConvoyerBeltSystem/Helpers.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+using namespace std; 
+
+class Helpers
+{
+	// Implement here different Helper Function such as: 
+	// Unit conversion
+	// Data import
+	// Unique string/word mannipulations
+	// etc. 
+};
+
---
+++ ConvoyerBeltSystem/TestFunctions.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class TestFunctions
+{
+	// Should be implemented: Maybe try TDD (test driven development)
+	void testTCPServer();
+	void testKeyPad();
+	void testPotentiometer();
+	void testMotor();
+
+};
+
