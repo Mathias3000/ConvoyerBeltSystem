64d728079ea8af738186cb51c6f29d0673783189
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-04-20 20:02:38 +0200

    Implemented TCPServer class

--- ConvoyerBeltSystem/TCPServer.h
+++ ConvoyerBeltSystem/TCPServer.h
@@ -1,6 +1,38 @@
 #pragma once
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 class TCPServer
 {
-	
+private: 
+
+	int port = 5555;
+	in_addr_t systemAddr = inet_addr("192.168.7.2");
+	in_addr_t convBeltAddr = inet_addr("91.0.0.7");
+
+	int listening;
+	sockaddr_in server;
+	sockaddr_in client;
+	socklen_t clientSize;
+	char host[NI_MAXHOST];
+	char svc[NI_MAXSERV];	// services
+	int clientSocket;
+	int buffersize = 128;
+	char buffer[];
+
+	int init();
+
+public: 
+	TCPServer();
+	~TCPServer();
+
+
 };
 
