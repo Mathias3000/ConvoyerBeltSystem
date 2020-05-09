d36fc4a0bcc171cf7fc51d5dda6e879192431af3
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-05-09 15:20:35 +0200

    included test for SM

--- ConvoyerBeltSystem/main.cpp
+++ ConvoyerBeltSystem/main.cpp
@@ -1,11 +1,12 @@
 #include <cstdio>
 #include "TestFunctions.h"
 
+
 int main()
 {
     printf("hello from ConvoyerBeltSystem!\n");
 
-    testKeyBoard();
+    testSystemManager();
 
     return 0;
 }
