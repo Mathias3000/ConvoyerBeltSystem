ea8dbcd6a90488e15bc392f3adae472b38acf528
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-05-09 17:04:22 +0200

    Implemented some functions with prinf

--- ConvoyerBeltSystem/MotorController.cpp
+++ ConvoyerBeltSystem/MotorController.cpp
@@ -1,1 +1,19 @@
 #include "MotorController.h"
+
+int MotorController::followProfile(int direction)
+{
+	cout << "Following speed profile with direction " << direction << ". " << endl;
+	return 0;
+}
+
+int MotorController::setSpeed(double speed)
+{
+	cout << "set speed to " << speed << " rpm" << endl;
+	return 0; 
+}
+
+int MotorController::setDirection(int direction)
+{
+	cout << "Direction set to " << direction << ". " << endl;
+	return 0;
+}
