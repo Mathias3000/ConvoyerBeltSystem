baff0acdc13c7fcfca90f736a4fd49090df845c1
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-05-10 17:28:30 +0200

    Implemented potentiometer class

--- ConvoyerBeltSystem/Potentiometer.h
+++ ConvoyerBeltSystem/Potentiometer.h
@@ -1,7 +1,34 @@
 #pragma once
+#include <string>
+#include <fcntl.h>
+
+extern "C" {
+	#include "spi.h"
+	#include "gpio.h"
+}
+
 class Potentiometer
 {
+private: 
+	spiPinmux* pinMux;
+	spiDescriptor* potiDesc;
+
+	uint16_t readValue = 0;
+	unsigned short controlByte = 0b10010000;	// from datasheet
+	int a = 0;
+
 public : 
 
+	Potentiometer();
+	int getValue();
+
 };
+
+// salam
+// i am fine
+// Cvid was bas
+// has to stay at home for 1 month no work no school
+// now better. had 15k cases now 1k new active cases
+// do you have school or work or are you still staying at home
+
 
