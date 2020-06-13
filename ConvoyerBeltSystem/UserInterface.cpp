7be2dd1ce30213737473c7b167b17e7ce90da8a5
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-06-13 19:00:31 +0200

    Added functions for implementation

--- ConvoyerBeltSystem/UserInterface.cpp
+++ ConvoyerBeltSystem/UserInterface.cpp
@@ -1,1 +1,17 @@
 #include "UserInterface.h"
+
+UserInterface::UserInterface()
+{
+	keyPad = new KeyPad();
+	potentiometer = new Potentiometer();
+}
+
+UserInterface::~UserInterface()
+{
+	delete this;
+}
+
+Command* UserInterface::parse()
+{
+	return nullptr;
+}
--- ConvoyerBeltSystem/UserInterface.h
+++ ConvoyerBeltSystem/UserInterface.h
@@ -1,8 +1,17 @@
 #pragma once
 #include "KeyPad.h"
 #include "Potentiometer.h"
+#include "ICommand.h"
 
-class UserInterface
+class UserInterface: public ICommand
 {
+private: 
+	KeyPad* keyPad;
+	Potentiometer* potentiometer;
+
+public: 
+	UserInterface();
+	~UserInterface();
+	Command* parse();
 };
 
