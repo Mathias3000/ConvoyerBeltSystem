366ac32dfc4c5acea7f63b753f83a913131e6fa5
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-06-13 18:59:51 +0200

    Implemented send function

--- ConvoyerBeltSystem/Network.cpp
+++ ConvoyerBeltSystem/Network.cpp
@@ -1,1 +1,31 @@
 #include "Network.h"
+
+Network::Network()
+{
+	rightConveyorBelt = new TCPClient();
+	leftConveyorBelt = new TCPServer();
+	master = new TCPServer();
+}
+
+Command* Network::parse()
+{
+	return nullptr;
+}
+
+void Network::send(string data, Destination destination)
+{
+	switch (destination)
+	{
+	case Network::LeftConveyorBelt:
+		leftConveyorBelt->sendData(data);
+		break;
+	case Network::RightConveyorBelt:
+		rightConveyorBelt->sendData(data);
+		break;
+	case Network::Master:
+		master->sendData(data);
+		break;
+	default:
+		break;
+	}
+}
--- ConvoyerBeltSystem/Network.h
+++ ConvoyerBeltSystem/Network.h
@@ -1,9 +1,23 @@
 #pragma once
-class Network
+#include "ICommand.h"
+#include "TCPClient.h"
+#include "TCPServer.h"
+
+class Network: public ICommand
 {
 private: 
+	TCPServer* leftConveyorBelt;
+	TCPClient* rightConveyorBelt;
+	TCPServer* master;
 
 public: 
 	Network();
+	Command* parse();
+	enum Destination {
+		LeftConveyorBelt, 
+		RightConveyorBelt, 
+		Master
+	};
+	void send(string data, Destination destination);
 };
 
