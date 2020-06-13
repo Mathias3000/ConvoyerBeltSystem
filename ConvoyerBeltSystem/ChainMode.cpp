cc52f2befde2ce131776465f1ececad16f10e5d2
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-06-13 19:01:46 +0200

    added send and recv function for implementation

--- ConvoyerBeltSystem/ChainMode.cpp
+++ ConvoyerBeltSystem/ChainMode.cpp
@@ -1,1 +1,14 @@
 #include "ChainMode.h"
+
+//ChainMode::ChainMode()
+//{
+//	network = new Network();
+//}
+
+void ChainMode::recv()
+{
+}
+
+void ChainMode::send(Command command)
+{
+}
--- ConvoyerBeltSystem/ChainMode.h
+++ ConvoyerBeltSystem/ChainMode.h
@@ -2,15 +2,19 @@
 #include "Mode.h"
 #include "Command.h"
 #include "string"
+#include "Network.h"
 
 using namespace std; 
 
 class ChainMode : public Mode
 {
 private: 
+	Network* network;
 
 public: 
-	int send(Command command);
-	int handleRequest(string ReadyWait);	// TODO: choose proper datatype for ready and wait status. maybe even a class for better usability
+	ChainMode();
+	void handleRequest(string ReadyWait);	// TODO: choose proper datatype for ready and wait status. maybe even a class for better usability
+	void recv();
+	void send(Command command);
 };
 
