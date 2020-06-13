350ede2c5434f014848da119875c4cc8a3091697
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-06-13 19:05:07 +0200

    Added recv virtual function

--- ConvoyerBeltSystem/Mode.cpp
+++ ConvoyerBeltSystem/Mode.cpp
@@ -1,1 +1,11 @@
 #include "Mode.h"
+
+//Mode::Mode()
+//{
+//	motorController = new MotorController();
+//	display = new Display();
+//	receivedCommand = new Command();
+//	currentCommandType = new UserInterface();	// default commandType
+//
+//}
+
--- ConvoyerBeltSystem/Mode.h
+++ ConvoyerBeltSystem/Mode.h
@@ -1,12 +1,21 @@
 #pragma once
 #include "Command.h"
+//#include "MotorController.h"
+//#include "Display.h"
+//#include "ICommand.h"
 
 class Mode
 {
 private: 
-	Command currentCommand;
+	Command* receivedCommand;
 
 public: 
-	virtual int recv() = 0;
+	//MotorController* motorController;
+	//Display* display;
+	//ICommand* currentCommandType;
+
+	// Mode();
+	virtual void recv() = 0;	// ICommand is used only for recv(): probably neccessary to change return type of recv()
+	virtual void send(Command command)  = 0;
 };
 
