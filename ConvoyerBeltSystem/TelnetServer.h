3fbe45d9942a3d6d431ff7d92efdbab002d0f583
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-06-17 08:56:30 +0200

    Added userInputThread handler

--- ConvoyerBeltSystem/TelnetServer.h
+++ ConvoyerBeltSystem/TelnetServer.h
@@ -1,23 +1,29 @@
 #pragma once
+#include <thread>
+
 #include "TCPServer.h"
 #include "ICommunication.h"
+
+#define SPEED_CMD "tel speed"
 
 class TelnetServer: public ICommunication
 {
 private: 
 	int telnetPort = 23;
 	TCPServer* telnetServer;
-	char* readData;
+	int speedBuffer = 0;
 
 public: 
 
-	double buffer = 0;		// buffer to save last speed value
+	// check if acually needed
+	double buffer2 = 0;		// buffer to save last speed value
 	bool dirBuffer = true;	// buffer to save last direction value: true = right & false = left
 	bool updateCommunicationType = false;
 
 	TelnetServer();
 	Command* parse();
 	void sendData(string data);
+	void handleUserInput();
 
 };
 
